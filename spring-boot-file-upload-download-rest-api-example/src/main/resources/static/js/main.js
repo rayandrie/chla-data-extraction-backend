058b15801cd0ec11f7e06916ec2fdb6d44053091
rayandrie <andrieja@usc.edu>
2019-10-10 01:39:10 -0700

    added step1 js file

--- spring-boot-file-upload-download-rest-api-example/src/main/resources/static/js/main.js
+++ spring-boot-file-upload-download-rest-api-example/src/main/resources/static/js/main.js
@@ -1,49 +1,5 @@
 'use strict';
 
-function onPageLoad() {
-  // Get all the State Code Information from ACS
-  $.ajax({
-    url: '/getAcsCityStateInfo',
-    method: 'GET',
-    dataType: 'json',
-    contentType: "application/json;charset=utf-8",
-    success: function(data) {
-      console.log("Status: ", data.status);
-      console.log("Message: ", data.message);
-    },
-    error: function(request, error) {
-      console.log(error);
-    }
-  });
-
-  // Sample POST Call
-  let data = {
-    listOfDetailedVariables: [
-      'Median Gross Rent as a % of Household Income - Renter-Occupied Households paying cash rent',
-      'GINI Index of Income Inequality Households'
-    ],
-    listOfSubjectVariables: [
-      '% of Total Population - Under 5 Years',
-      '% of Total Households, Renter-Occupied - Household Size'
-    ]
-  };
-  data = JSON.stringify(data);
-
-  $.ajax({
-    url: '/appendACSVariables',
-    method: 'POST',
-    data: data,
-    contentType: "application/json;charset=utf-8",
-    success: function(data) {
-      console.log("Status: ", data.status);
-      console.log("Message: ", data.message);
-    },
-    error: function(request, error) {
-      console.log(error);
-    }
-  });
-}
-
 function uploadSingleFile(file) {
     var formData = new FormData();
     formData.append("file", file);
@@ -107,8 +63,6 @@ var multipleFileUploadError = document.querySelector('#multipleFileUploadError')
 var multipleFileUploadSuccess = document.querySelector('#multipleFileUploadSuccess');
 
 // var testButton = document.querySelector('#tester');
-
-onPageLoad();
 
 // testButton.addEventListener('click', function(event) {
 //   console.log("Clicked Test Button!");
