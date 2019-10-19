93bbe8f23e088d6562e4c41e55d297881cd24bfa
sophiahu12 <husophia@usc.edu>
2019-10-18 22:58:48 -0700

    seperates subject and detailed parameters

--- spring-boot-file-upload-download-rest-api-example/src/main/java/com/example/filedemo/controller/FileController.java
+++ spring-boot-file-upload-download-rest-api-example/src/main/java/com/example/filedemo/controller/FileController.java
@@ -1,7 +1,11 @@
 package com.example.filedemo.controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +16,7 @@ import com.example.filedemo.payload.UploadFileResponse;
 import com.example.filedemo.request.AcsVariablesRequest;
 import com.example.filedemo.request.BmiInfoRequest;
 import com.example.filedemo.request.SsdiRequest;
+import com.example.filedemo.response.acs.config.Variables;
 import com.example.filedemo.response.jewishgen.SsdiObject;
 import com.example.filedemo.response.zscore.BmiInfoResponse;
 import com.example.filedemo.service.AcsApiService;
@@ -36,16 +41,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.*;
-import java.util.List;
-import java.nio.file.Path;
-import java.nio.file.Paths;;
 
 @RestController
 public class FileController {
@@ -156,9 +151,12 @@ public class FileController {
       writer.append(",");
 
       // for all parameters, add a column for each
-      for (String parameter : parameters) {
-        writer.append(parameter);
-        writer.append(",");
+      if (!results.isEmpty()) {
+        List<ImmutablePair<String, String>> column = (List<ImmutablePair<String, String>>) results.values().toArray()[0];
+        for (ImmutablePair<String, String> p : column ) {
+          writer.append(p.left);
+          writer.append(",");
+        }
       }
 
       writer.append("\n");
@@ -170,8 +168,10 @@ public class FileController {
         ArrayList<String> line = new ArrayList<>();
 
         if (content.contains(entry.getKey())) {
-          line.add(entry.getKey());
-          line.add(entry.getValue().get(0).right); // this only gets one parameter?
+          line.add(entry.getKey());  //adds the state 
+          for (ImmutablePair<String,String> pair : entry.getValue()) { //adds all the parameter values
+              line.add(pair.right); 
+          }
           csv.add(line);
         }
       }
@@ -190,18 +190,22 @@ public class FileController {
 
   // ACS specific - split parameters into detailed and subject
   public AcsVariablesRequest splitACSVars(ArrayList<String> parameters) {
-
-    // TOOD for each parameter check if in detailed or subject
-
     AcsVariablesRequest req = new AcsVariablesRequest();
-    Object[] parametersArray = parameters.toArray();
-    String[] listOfDetailedVariables = Arrays.copyOf(parametersArray, parametersArray.length, String[].class);
-    req.setListOfDetailedVariables(listOfDetailedVariables);
+    ArrayList<String> listOfDetailedVariables = new ArrayList<>();
+    ArrayList<String> listOfSubjectVariables = new ArrayList<>();
 
-    // set subject list to empty bc it is a detailed query - will need to
-    // differentiate later
-    String[] listOfSubjectVariables = {};
-    req.setListOfSubjectVariables(listOfSubjectVariables);
+    //if parameter starts with S it is a Subject query
+    for (String parameter : parameters) {
+      String varId = Variables.VAR_ID_BY_NAME.get(parameter);
+      if (varId.charAt(0) == 'S') {
+        listOfSubjectVariables.add(parameter);
+      } else {
+        listOfDetailedVariables.add(parameter);
+      }
+    }
+
+    req.setListOfDetailedVariables(listOfDetailedVariables.toArray(new String[listOfDetailedVariables.size()]));
+    req.setListOfSubjectVariables(listOfSubjectVariables.toArray(new String[listOfSubjectVariables.size()]));
 
     return req;
   }
@@ -239,6 +243,8 @@ public class FileController {
     // append acs data to user's file - using example query first
     ArrayList<String> parameters = new ArrayList<>();
     parameters.add("GINI Index of Income Inequality Households");
+    parameters.add("Median Gross Rent as a % of Household Income - Renter-Occupied Households paying cash rent");
+    parameters.add("Age Dependency Ratio");
 
     acs(resource, parameters, content);
 
