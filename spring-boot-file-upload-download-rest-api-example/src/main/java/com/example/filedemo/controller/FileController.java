fe496cd6fe79fa846a7c36f12033881e4d460c96
sophiahu12 <husophia@usc.edu>
2019-10-18 20:32:07 -0700

    acs file output to csv wtih parameters

--- spring-boot-file-upload-download-rest-api-example/src/main/java/com/example/filedemo/controller/FileController.java
+++ spring-boot-file-upload-download-rest-api-example/src/main/java/com/example/filedemo/controller/FileController.java
@@ -22,12 +22,13 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import javax.servlet.http.HttpServletRequest;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.*; 
+import java.util.*;
 import java.util.List;
 import java.nio.file.Path;
 import java.nio.file.Paths;;
@@ -35,196 +36,189 @@ import java.nio.file.Paths;;
 @RestController
 public class FileController {
 
-    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
-    private String filename;
-    MultipartFile file;
+  private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+  private String filename;
+  MultipartFile file;
 
-    @Autowired
-    private FileStorageService fileStorageService;
-    
-    @Autowired
-    private AcsApiService acsApiService;
+  @Autowired
+  private FileStorageService fileStorageService;
 
-    @Autowired
-    private SsdiService ssdiService;
+  @Autowired
+  private AcsApiService acsApiService;
 
-    @GetMapping("/testing")
-    public BasicResponse testing() {
-      return new BasicResponse(201, "Success", "Hit /testing endpoint");
+  @Autowired
+  private SsdiService ssdiService;
+
+  @GetMapping("/testing")
+  public BasicResponse testing() {
+    return new BasicResponse(201, "Success", "Hit /testing endpoint");
+  }
+
+  @PostMapping("/getSsdiInfo")
+  public List<SsdiObject> getSsdiInfo(@RequestBody SsdiRequest req) {
+    List<SsdiObject> response = ssdiService.getRecordsByName(req.getFirstName(), req.getLastName());
+
+    return response;
+  }
+
+  @GetMapping("/getAcsCityStateInfo")
+  public BasicResponse getAcsCityStateInfo() {
+    acsApiService.initAcsInfo();
+
+    return new BasicResponse(201, "Success", "Received all City State Information");
+  }
+
+  @PostMapping("/appendACSVariables")
+  public BasicResponse appendACSVariables(@RequestBody AcsVariablesRequest req) {
+    // Make a request to the ACS API if there are variables to be searched for
+    if (req.detailedVariablesIsEmpty() && req.subjectVariablesIsEmpty()) {
+      return new BasicResponse(404, "Failure", "Variable Lists empty, no Request can be made.");
     }
 
-    @PostMapping("/getSsdiInfo")
-    public List<SsdiObject> getSsdiInfo(@RequestBody SsdiRequest req) {
-      List<SsdiObject> response = ssdiService.getRecordsByName(req.getFirstName(), req.getLastName());
+    return new BasicResponse(201, "Success", "Successfully made ACS Request.");
+  }
 
-      return response;
-    }
+  @PostMapping("/uploadFile")
+  public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+    String fileName = fileStorageService.storeFile(file);
+    this.file = file;
+    filename = fileName;
 
-    @GetMapping("/getAcsCityStateInfo")
-    public BasicResponse getAcsCityStateInfo() {
-      acsApiService.initAcsInfo();
+    String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/downloadFile/").path(fileName)
+        .toUriString();
 
-      return new BasicResponse(201, "Success", "Received all City State Information");
-    }
+    return new UploadFileResponse(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+  }
 
-    @PostMapping("/appendACSVariables")
-    public BasicResponse appendACSVariables(@RequestBody AcsVariablesRequest req) {
-      // Make a request to the ACS API if there are variables to be searched for
-      if (req.detailedVariablesIsEmpty() && req.subjectVariablesIsEmpty()) {
-        return new BasicResponse(404, "Failure", "Variable Lists empty, no Request can be made.");
+  @PostMapping("/returnDownloadFile")
+  public UploadFileResponse returnDownloadFile() {
+
+    String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/downloadFile/").path(filename)
+        .toUriString();
+
+    return new UploadFileResponse(filename, fileDownloadUri, file.getContentType(), file.getSize());
+  }
+
+  // take current user inputted csv and converts it into an ArrayList<String>
+  public ArrayList<String> csvtoList(Resource resource) {
+    ArrayList<String> records = new ArrayList<>();
+    try (BufferedReader br = new BufferedReader(new FileReader(resource.getFile()))) {
+      String line;
+
+      while ((line = br.readLine()) != null) {
+        records.add(line);
       }
 
-      return new BasicResponse(201, "Success", "Successfully made ACS Request.");
+    } catch (FileNotFoundException fne) {
+      System.out.println(fne);
+
+    } catch (IOException e) {
+      System.out.println(e);
     }
 
-    @PostMapping("/uploadFile")
-    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
-        String fileName = fileStorageService.storeFile(file);
-        this.file = file;
-        filename = fileName;
+    return records;
+  }
 
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
-                .path(fileName)
-                .toUriString();
+  public void populateCSV(Resource resource, ArrayList<String> parameters,
+      Map<String, List<ImmutablePair<String, String>>> results, ArrayList<String> content) {
+    // write the results data to csv - just write it to file for now
+    try {
+      FileWriter writer = new FileWriter(resource.getFile(), false);
 
-        return new UploadFileResponse(fileName, fileDownloadUri,
-                file.getContentType(), file.getSize());
-    }
+      // add new parameters - define column headers here
+      writer.append("State");
+      writer.append(",");
 
-    @PostMapping("/returnDownloadFile")
-    public UploadFileResponse returnDownloadFile() {
-
-      String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/downloadFile/")
-                .path(filename)
-                .toUriString();
-
-        return new UploadFileResponse(filename, fileDownloadUri,
-                file.getContentType(), file.getSize());
-    }
-
-    //take current user inputted csv and converts it into an ArrayList<String>
-    public List<String> csvtoList(Resource resource) {
-      List<String> content = Collections.emptyList();
-      try {
-        Path path = Paths.get(resource.getURI());
-        content = Files.readAllLines(path);
-
-        for (String line : content) {
-          System.out.println(line);
-        }
-
-      } catch (IOException e) {
-        System.out.println(e);
+      // for all parameters, add a column for each
+      for (String parameter : parameters) {
+        writer.append(parameter);
+        writer.append(",");
       }
 
-      return content;
-    }
+      writer.append("\n");
 
-    public void populateCSV(Resource resource, ArrayList<String> parameters, Map<String, List<ImmutablePair<String, String> > > results, List<String> content) {
-            //write the results data to csv - just write it to file for now
-            try {
-              FileWriter writer = new FileWriter(resource.getFile(), false); 
-             
-              //add new parameters - define column headers here 
-              writer.append("State");     
-              writer.append(","); 
-              
-              //for all parameters, add a column for each
-              for (String parameter : parameters) {
-                writer.append(parameter);
-                writer.append(","); 
-              }
-              
-              writer.append("\n"); 
+      // combine user inputted content with API results
+      ArrayList<ArrayList<String>> csv = new ArrayList<>();
 
-              //combine user inputted content with API results 
-              ArrayList<ArrayList<String>> csv = new ArrayList<>();
-            
-              for (Map.Entry<String, List<ImmutablePair<String, String>>> entry : results.entrySet()) {
-                ArrayList<String> line = new ArrayList<>();
-                if (content.contains(entry.getKey())) {
-                  line.add(entry.getKey());
-                  line.add(entry.getValue().get(0).right); // this only gets one parameter?
-                  csv.add(line);
-                }
-              }
+      for (Map.Entry<String, List<ImmutablePair<String, String>>> entry : results.entrySet()) {
+        ArrayList<String> line = new ArrayList<>();
 
-              for (ArrayList<String> al : csv) {
-                writer.append(String.join(",", al));
-                writer.write(System.getProperty("line.separator"));
-              }
-    
-              writer.flush();
-              writer.close();
-             }catch (IOException e) { 
-                 e.printStackTrace(); 
-             } 
-    }
-
-    //ACS specific - split parameters into detailed and subject 
-    public AcsVariablesRequest splitACSVars(ArrayList<String> parameters) {
-
-      //TOOD for each parameter check if in detailed or subject 
-
-
-      AcsVariablesRequest req = new AcsVariablesRequest();
-      Object[] parametersArray = parameters.toArray(); 
-      String[] listOfDetailedVariables = Arrays.copyOf(parametersArray, parametersArray.length, String[].class);
-      req.setListOfDetailedVariables(listOfDetailedVariables);    
-
-      //set subject list to empty bc it is a detailed query - will need to differentiate later
-      String[] listOfSubjectVariables = {};
-      req.setListOfSubjectVariables(listOfSubjectVariables);
-
-      return req;
-    }
-
-    //process the acs database queries
-    public void acs(Resource resource, ArrayList<String> parameters, List<String> content) {
-        
-        AcsVariablesRequest req = splitACSVars(parameters);
-
-        Map<String, List<ImmutablePair<String, String> > > results = acsApiService.makeAcsGetRequest(req); 
-        if (results != null) {
-          System.out.println(results);
+        if (content.contains(entry.getKey())) {
+          line.add(entry.getKey());
+          line.add(entry.getValue().get(0).right); // this only gets one parameter?
+          csv.add(line);
         }
+      }
 
-      populateCSV(resource, parameters, results, content);
+      for (ArrayList<String> al : csv) {
+        writer.append(String.join(",", al));
+        writer.write(System.getProperty("line.separator"));
+      }
+
+      writer.flush();
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  // ACS specific - split parameters into detailed and subject
+  public AcsVariablesRequest splitACSVars(ArrayList<String> parameters) {
+
+    // TOOD for each parameter check if in detailed or subject
+
+    AcsVariablesRequest req = new AcsVariablesRequest();
+    Object[] parametersArray = parameters.toArray();
+    String[] listOfDetailedVariables = Arrays.copyOf(parametersArray, parametersArray.length, String[].class);
+    req.setListOfDetailedVariables(listOfDetailedVariables);
+
+    // set subject list to empty bc it is a detailed query - will need to
+    // differentiate later
+    String[] listOfSubjectVariables = {};
+    req.setListOfSubjectVariables(listOfSubjectVariables);
+
+    return req;
+  }
+
+  // process acs database queries
+  public void acs(Resource resource, ArrayList<String> parameters, ArrayList<String> content) {
+
+    AcsVariablesRequest req = splitACSVars(parameters);
+
+    Map<String, List<ImmutablePair<String, String>>> results = acsApiService.makeAcsGetRequest(req);
+
+    populateCSV(resource, parameters, results, content);
+  }
+
+  @GetMapping("/downloadFile/{fileName:.+}")
+  public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+    // Load file as Resource
+    Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+    // Try to determine file's content type
+    String contentType = null;
+    try {
+      contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+    } catch (IOException ex) {
+      logger.info("Could not determine file type.");
     }
 
-    @GetMapping("/downloadFile/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-        // Load file as Resource
-        Resource resource = fileStorageService.loadFileAsResource(fileName);
-
-        // Try to determine file's content type
-        String contentType = null;
-        try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-        } catch (IOException ex) {
-            logger.info("Could not determine file type.");
-        }
-
-        // Fallback to the default content type if type could not be determined
-        if(contentType == null) {
-            contentType = "application/octet-stream";
-        }
-
-        //copies current data in csv back into csv (avoids formatting errors)
-        List<String> content = csvtoList(resource);
-
-        //append acs data to user's file - using example query first
-        ArrayList<String> parameters = new ArrayList<>();
-        parameters.add("GINI Index of Income Inequality Households");
-        
-        acs(resource, parameters, content);
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
+    // Fallback to the default content type if type could not be determined
+    if (contentType == null) {
+      contentType = "application/octet-stream";
     }
+
+    ArrayList<String> content = csvtoList(resource);
+
+    // append acs data to user's file - using example query first
+    ArrayList<String> parameters = new ArrayList<>();
+    parameters.add("GINI Index of Income Inequality Households");
+
+    acs(resource, parameters, content);
+
+    return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+        .body(resource);
+  }
 
 }
